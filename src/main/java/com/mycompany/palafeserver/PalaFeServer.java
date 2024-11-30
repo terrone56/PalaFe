511654a9bd7fbcc9aca2c7a47cd671c56c13d54f
marzola.s <marzola.s@A12-06.intra.iis.it>
2024-11-30 09:09:37 +0100

    primo commit del proggetto

---
+++ src/main/java/com/mycompany/palafeserver/PalaFeServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.palafeserver;
+
+/**
+ *
+ * @author marzola.s
+ */
+public class PalaFeServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
